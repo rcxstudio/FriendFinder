649e1cd266195beecc25636b1753d87caf506c7d
HQ <Spawn@ROPERs-MacBook-Pro.local>
2017-07-23 15:15:07 -0700

    Adding images folder for default characters

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-var express = require("express");
-var bodyParser = require("body-parser");
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path')
 
 var app = express();
 var PORT = 8080;
@@ -7,11 +8,11 @@ var PORT = 8080;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
-require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlRoutes")(app);
+require('./app/routing/apiRoutes')(app);
+require('./app/routing/htmlRoutes')(app);
 
 app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
+  console.log('App listening on PORT: ' + PORT);
 });
