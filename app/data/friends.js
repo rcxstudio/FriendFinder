67ffc733ab8ba688d901b49a56f237b825620195
HQ <Spawn@ROPERs-MacBook-Pro.local>
2017-07-23 15:44:36 -0700

    adding friends and info

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+var friends = [
+  {
+    name: 'Superman',
+    photo: '../../images/superman.jpg',
+    scores: [3, 3, 5, 5, 5, 1, 1, 5, 5, 4]
+  {
+    name: 'Batman',
+    photo: '../../images/batman.jpg',
+    scores: [1, 3, 5, 5, 1, 5, 5, 1, 1, 1]
+  },
+  {
+    name: 'Wonder Woman',
+    photo: '../../images/wonder-woman.jpg',
+    scores: [1, 3, 5, 5, 1, 5, 5, 1, 1, 2]
+  },
+  {
+    name: 'Lois Lane',
+    photo: '../../images/lois-lane.jpg',
+    scores: [3, 3, 5, 4, 5, 1, 1, 5, 5, 4]
+  }
+];
