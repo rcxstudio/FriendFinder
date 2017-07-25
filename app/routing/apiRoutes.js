89adb984937cbc312102078ac12b529e8fb08918
Roper Macaraeg <5462117+Ropestar1@users.noreply.github.com>
2017-07-24 22:23:04 -0700

    testing apiroutes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,23 +1,15 @@
-var path = require('path');
-var friends = require("../data/friends.js"); //do you need .js extension?
+// var path = require('path');
+var friends = require("../data/friends.js");
 
 module.exports = function(app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
-
   app.get("/api/friends", function(req, res) {
     res.json(friends);
   });
 
   app.post("/api/friends", function(req, res) {
-    var newFriend = req.body;
+    // var newFriend = res.body;
     console.log(newFriend)
     console.log('res from post', res);
     console.log('newFriend', newFriend);
-
-
   });
 };
