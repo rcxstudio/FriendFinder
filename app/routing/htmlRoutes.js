f6c130b8a705f0cefb0b917e3cf30f0a19aeea13
Roper Macaraeg <5462117+Ropestar1@users.noreply.github.com>
2017-07-24 20:46:29 -0700

    fixing button

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -6,16 +6,16 @@ module.exports = function(app) {
   // In each of the below cases the user is shown an HTML page of content
   // ---------------------------------------------------------------------------
 
-  // app.get("/", function(req, res) {
-  //   res.sendFile(path.join(__dirname, "/../public/home.html"));
-  // });
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/home.html"));
+  });
 
   app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "/../public/survey.html"));
   });
 
   // If no matching route is found default to home
-  app.use(function(req, res) {
-    res.sendFile(path.join(__dirname, "/../public/home.html"));
-  });
+  // app.use(function(req, res) {
+  //   res.sendFile(path.join(__dirname, "/../public/home.html"));
+  // });
 };
