0d8c4337402acd5b84ea1efa1abb2aeab24baf1c
Roper Macaraeg <5462117+Ropestar1@users.noreply.github.com>
2017-07-24 21:52:25 -0700

    working on post

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,24 +11,11 @@ module.exports = function(app) {
     res.json(friends);
   });
 
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
+  app.post("/api/friends", function(req, res) {
+    var newFriend = req.body;
+    console.log('res from post', res);
+    console.log('newFriend', newFriend);
 
-  // app.post("/api/tables", function(req, res) {
-  //   // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-  //   // It will do this by sending out the value "true" have a table
-  //   if (tableData.length < 5) {
-  //     tableData.push(req.body);
-  //     res.json(true);
-  //   }
-  //   else {
-  //     waitListData.push(req.body);
-  //     res.json(false);
-  //   }
-  // });
+
+  });
 };
