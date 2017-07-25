b68e06286296bcbffb91db53efaff489ac71bea8
Roper Macaraeg <5462117+Ropestar1@users.noreply.github.com>
2017-07-24 20:11:23 -0700

    Adding files

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('./routing/')
 })
 
 app.use(bodyParser.json());
