1fdd71d85209480eea8f73b7a7e4fbbffaeaced5
Roper Macaraeg <5462117+Ropestar1@users.noreply.github.com>
2017-07-26 08:49:46 -0700

    Functional with minor image bugs

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,22 +1,23 @@
 var friends = [
   {
     name: 'Superman',
-    photo: './../../images/superman.jpg',
+    photo: 'https://cdn.theatlantic.com/assets/media/img/mt/2016/01/superman/facebook.jpg?1454104448',
     scores: [3, 3, 5, 5, 5, 1, 1, 5, 5, 4]
   },
   {
     name: 'Batman',
-    photo: './../../images/batman.jpg',
+    photo: 'https://static.comicvine.com/uploads/scale_small/11/113509/4693444-6164752601-ben_a.jpg',
     scores: [1, 3, 5, 5, 1, 5, 5, 1, 1, 1]
   },
   {
     name: 'Wonder Woman',
-    photo: './../../images/wonder-woman.jpg',
+    photo: 'http://cdn.collider.com/wp-content/uploads/2017/03/justice-league-wonder-woman-poster.jpg',
     scores: [1, 3, 5, 5, 1, 5, 5, 1, 1, 2]
   },
   {
     name: 'Lois Lane',
-    photo: './../../images/lois-lane.jpg',
+    photo: 'https://upload.wikimedia.org/wikipedia/en/9/9d/Loissmall.jpg',
+    // ../../images/lois-lane.jpg //Why won't this link work?
     scores: [3, 3, 5, 4, 5, 1, 1, 5, 5, 4]
   }
 ];
--- server.js
+++ server.js
@@ -2,8 +2,6 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 
-var friends = require('./app/data/friends.js');
-
 var app = express();
 var PORT = process.env.PORT || 8080;
 
