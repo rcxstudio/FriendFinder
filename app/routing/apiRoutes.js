af1feb68c9d012579d75f6fb9c3b0c8956940e63
Roper Macaraeg <5462117+Ropestar1@users.noreply.github.com>
2017-07-25 23:50:46 -0700

    almost working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,8 +7,34 @@ module.exports = function(app) {
   });
 
   app.post("/api/friends", function(req, res) {
-    res.send(req);
-    // var newFriend = req.body;
-    // friends.push(newFriend);
+    console.log('req.body', req.body);
+    // console.log('req', req);
+    // console.log('res', res);
+
+    var newFriend = req.body;
+    friends.push(newFriend);
+    var lastFriendIndexValue = friends.length - 1;
+    console.log(friends);
+    var totalDifference = 200;
+    var tempScore = 0;
+    var bestFriend;
+    for (var i = 0; i < lastFriendIndexValue; i++) {
+      for (var j = 0; j < friends[i].scores.length; j++) {
+        tempScore += (Math.abs(parseInt(friends[i].scores[j]) - parseInt(friends[lastFriendIndexValue].scores[j])));
+        // console.log(friends[i].scores[j]);
+        // console.log('addedFriend', friends[lastFriendIndexValue].scores[j]);
+      }
+
+      if (tempScore < totalDifference) {
+        totalDifference = tempScore;
+        bestFriend = friends[i];
+        console.log('Best Friend', bestFriend);
+        // return bestFriend;
+      }
+      console.log('tempScore', tempScore);
+      tempScore = 0;
+    }
+
+    res.send(bestFriend);
   });
 };
