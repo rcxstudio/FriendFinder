bfb80343f1836802f148537c161f737beb3d5ed2
Roper Macaraeg <5462117+Ropestar1@users.noreply.github.com>
2017-07-26 08:07:25 -0700

    Fixing modal image issue

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,22 +1,22 @@
 var friends = [
   {
     name: 'Superman',
-    photo: '../../images/superman.jpg',
+    photo: './../../images/superman.jpg',
     scores: [3, 3, 5, 5, 5, 1, 1, 5, 5, 4]
   },
   {
     name: 'Batman',
-    photo: '../../images/batman.jpg',
+    photo: './../../images/batman.jpg',
     scores: [1, 3, 5, 5, 1, 5, 5, 1, 1, 1]
   },
   {
     name: 'Wonder Woman',
-    photo: '../../images/wonder-woman.jpg',
+    photo: './../../images/wonder-woman.jpg',
     scores: [1, 3, 5, 5, 1, 5, 5, 1, 1, 2]
   },
   {
     name: 'Lois Lane',
-    photo: '../../images/lois-lane.jpg',
+    photo: './../../images/lois-lane.jpg',
     scores: [3, 3, 5, 4, 5, 1, 1, 5, 5, 4]
   }
 ];
